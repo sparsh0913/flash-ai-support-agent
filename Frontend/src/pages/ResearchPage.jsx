ac43ee33b73c47397a1c59526b58e3aec0e82984
Sparsh Agarwal <sparshagarwal190@gmail.com>
2026-05-16 22:17:01 +0530

    Fixing response for guest user

--- Frontend/src/pages/ResearchPage.jsx
+++ Frontend/src/pages/ResearchPage.jsx
@@ -121,7 +121,7 @@ const handleSend =  async ()=>{
         if(data.type === "ai"){
           setStatus("");
           setLoading(false);
-         /*   if(!user){
+            if(!user){
       setMessages((prev)=>[
          ...prev,
          {
@@ -129,7 +129,7 @@ const handleSend =  async ()=>{
             content:data.payload.text
          }
       ]);
-   } */
+   } 
           if(currentChatId && user){
               fetchChatMessages(currentChatId);
           }
